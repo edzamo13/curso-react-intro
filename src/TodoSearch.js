64f0d01efa905d93f8334e2d2b9866866e4ed09f
edzamo13@gmail.com <ezamora@mag.gob.ec>
2023-05-08 12:17:43 -0500

    example console

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,7 +1,18 @@
-import './css/TodoSearch.css'
+import "./css/TodoSearch.css";
 
 function TodoSearch() {
-  return <input placeholder="enter any word" className='TodoSearch'></input>;
+  return (
+    <input
+      placeholder="enter any word"
+      className="TodoSearch"
+      onChange={(event) => {
+        console.log("You write something ");
+        console.log("event", event);
+        console.log("event.target", event.target);
+        console.log("event.target.value" , event.target.value);
+      }}
+    ></input>
+  );
 }
 
 export { TodoSearch };
